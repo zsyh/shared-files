4051669b144c329063aadf6f116ce12c01510a3a
张单旸晖 <236294386@qq.com>
2017-07-17 14:43:10 +0800

    add centos install docker shell

---
+++ centos7_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+user="zaizai"
+curl -sSL https://get.docker.com/ | sh
+usermod -aG docker $user
+systemctl enable docker
+# docker pull zsyh/spark:2.1.1
+yum install -y git
+cd /tmp
+git clone https://github.com/zsyh/shared-files.git
+cp shared-files/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+cp shared-files/weave /usr/local/bin/weave
+chmod +x /usr/local/bin/weave
+cd /home/$user
+git clone https://github.com/zsyh/docker-spark.git
